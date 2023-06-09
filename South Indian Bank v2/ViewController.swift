dd1b38f31b3c610228b9631c7a26933fcb3ba5f1
Swantan Barua <work.swantan@gmail.com>
2023-06-09 10:12:35 +0530

    Initial Commit

---
+++ South Indian Bank v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  South Indian Bank v2
+//
+//  Created by VisioApps on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

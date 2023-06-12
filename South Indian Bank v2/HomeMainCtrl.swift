e3c4cc0978f63e5b774f2cbf0d58300b8690027a
Swantan Barua <work.swantan@gmail.com>
2023-06-12 17:10:43 +0530

    Home Screen labels and IBActions added

---
+++ South Indian Bank v2/HomeMainCtrl.swift
@@ -0,0 +1,104 @@
+//
+//  HomeMainCtrl.swift
+//  South Indian Bank v2
+//
+//  Created by Swantan Barua on 09/06/23.
+//
+
+import UIKit
+
+class HomeMainCtrl: UIViewController {
+    
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    @IBOutlet weak var imgprofile: UIImageView!
+    @IBOutlet weak var btnnotif: UIButton!
+    @IBOutlet weak var btnsearch: UIButton!
+    @IBOutlet weak var btnlogout: UIButton!
+    @IBOutlet weak var lblacnts: UILabel!
+    @IBOutlet weak var lblgreetmsg: UILabel!
+    @IBOutlet weak var lblname: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        cardCollectionView.delegate = self
+//        cardCollectionView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    @IBAction func calKYC(_ sender: UIButton) {
+    }
+    
+    @IBAction func calllogout(_ sender: UIButton) {
+    }
+    
+    @IBAction func launchfaq(_ sender: UIButton) {
+    }
+    
+    @IBAction func launchpayments(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadCprChequeBookPopup(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadBankholiday(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadCprRequestServicesView(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadDeposit(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadEmical(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadaccount(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadcomplaints(_ sender: UIButton) {
+    }
+    
+    @IBAction func loaddebitcard(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadtranhist(_ sender: UIButton) {
+    }
+    
+    @IBAction func openCreditcard(_ sender: UIButton) {
+    }
+    
+    @IBAction func openOotwardRemit(_ sender: UIButton) {
+    }
+    
+    @IBAction func openUPISDK(_ sender: UIButton) {
+    }
+    
+    @IBAction func opencolfee(_ sender: UIButton) {
+    }
+    
+    @IBAction func openfinanciltool(_ sender: UIButton) {
+    }
+    
+    @IBAction func openloan(_ sender: UIButton) {
+    }
+    
+    @IBAction func openoffering(_ sender: UIButton) {
+    }
+    
+    @IBAction func openonlipymt(_ sender: UIButton) {
+    }
+}
+
+extension HomeMainCtrl: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = cardCollectionView.dequeueReusableCell(withReuseIdentifier: "CardCollectionViewCell", for: indexPath) as! CardCollectionViewCell
+        return cell
+    }
+}

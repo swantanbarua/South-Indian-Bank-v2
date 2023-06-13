5f4fdabf5b884859247ebb95a62ac5b06229e985
Swantan Barua <work.swantan@gmail.com>
2023-06-13 17:19:40 +0530

    Mini Statement Header View completed

--- South Indian Bank v2/MiniStatementCtrl.swift
+++ South Indian Bank v2/MiniStatementCtrl.swift
@@ -2,15 +2,23 @@
 //  MiniStatementCtrl.swift
 //  South Indian Bank v2
 //
-//  Created by VisioApps on 13/06/23.
+//  Created by Swantan Barua on 13/06/23.
 //
 
 import UIKit
 
 class MiniStatementCtrl: UIViewController {
 
+    @IBOutlet weak var miniStatementlbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        miniStatementlbl.layer.borderWidth = 0.2
+        miniStatementlbl.layer.borderColor = .init(gray: 255/255, alpha: 1)
+        miniStatementlbl.layer.cornerRadius = 10
     }
     
     @IBAction func backBtn(_ sender: UIButton) {
---
+++ South Indian Bank v2/MiniStatementTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MiniStatementTableViewCell.swift
+//  South Indian Bank v2
+//
+//  Created by Swantan Barua on 13/06/23.
+//
+
+import UIKit
+
+class MiniStatementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}

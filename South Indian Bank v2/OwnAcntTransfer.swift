ca8be0c60294b1a1627f1e68f6e8865071804893
Swantan Barua <work.swantan@gmail.com>
2023-06-15 18:13:37 +0530

    My SIB Account Header View

---
+++ South Indian Bank v2/OwnAcntTransfer.swift
@@ -0,0 +1,15 @@
+//
+//  OwnAcntTransfer.swift
+//  South Indian Bank v2
+//
+//  Created by Swantan Barua on 15/06/23.
+//
+
+import UIKit
+
+class OwnAcntTransfer: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
